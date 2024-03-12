39e187f7ff23f66e5a0ecfef9170329aecf13b29
zaldanaraul <raularmandozc12@gmail.com>
2024-03-12 16:21:48 -0400

    add documentation to bg.tmpfile function

--- bgrim.bash
+++ bgrim.bash
@@ -705,12 +705,27 @@ bg.trap() {
        } >&2
 }
 
+# description: |
+#   Creates a temporary file using 'mktemp' and sets an EXIT trap for
+#   the file to be deleted upon exit of the current shell process. It
+#   takes the name of a variable as it's only argument and places the
+#   path to the temporary file in the variable whose name is provided.
+# inputs:
+#   stdin:
+#   args:
+#     1: filename variable
+# outputs:
+#   stderr:
+#   return_code:
+#     0: "if the file was successfully created and the exit trap set"
+#     1: "if there was an error while creating the file or setting the trap"
+# dependencies:
+# tags:
+#   - "error_handling" 
 bg.tmpfile() {
   # Verify arguments
   [[ -z "${1:-}" ]] \
     && { echo "ERROR: arg1 (filename_var) not provided but required" >&2; return 1; }
-
-
 
   local filename_var
   filename_var="$1"
@@ -726,7 +741,6 @@ bg.tmpfile() {
     || { echo "ERROR: Unable to create temporary file" >&2; return 1; }
   bg.trap "rm -f '$tmpfile_name'" 'EXIT' \
     || { echo "ERROR: Unable to set exit trap to delete file '$tmpfile_name'" >&2; return 1; }
-
 
   # Assign name of tmpfile to variable whose name is contained in
   # filename_var
