532ad6f4db8632593f93552eb5dd26f858097365
Raul <raularmandozc12@gmail.com>
2024-11-07 13:32:47 -0500

    put all env functions together

--- core.bash
+++ core.bash
@@ -315,6 +315,10 @@ bg.str.is_valid_bash_opt() (
   return 1
 )
 
+################################################################################
+# INPUT FUNCTIONS
+################################################################################
+
 # description: |
 #   This function is meant to ensure that a function receives all the arguments
 #   it expects. It expects an array called 'required_args' to be set in its
@@ -478,9 +482,10 @@ bg.in.require_args() {
 
 }
 
-
-
 ################################################################################
+# ENVIRONMENT FUNCTIONS
+################################################################################
+
 # description: |
 #   Clears all options in the environment that can be set with both the 'set' 
 #   and the 'shopt' built-in commands 
@@ -494,7 +499,6 @@ bg.in.require_args() {
 #     0: "always"
 # tags:
 #   - "changes env"
-################################################################################
 bg.env.clear_shell_opts() {
   # Clear all options set with the 'set' built-in
   while read -r option_name option_status; do
@@ -507,6 +511,121 @@ bg.env.clear_shell_opts() {
   done < <( shopt )
 }
 
+# description: |
+#   Clears all variables in the environment that start with the given prefix.
+#   Will unset global shell variables, as well as local and environment 
+#   variables. Will return an error if the provided prefix is not a valid
+#   function name (i.e. it's empty or not composed entirely of alphanumeric
+#   characters and underscores)
+# inputs:
+#   stdin:
+#   args:
+#     1: "prefix"
+# outputs:
+#   stdout:
+#   stderr: "Error message if prefix is not valid"
+#   return_code:
+#     0: "All variables with the given prefix were unset from the environment"
+#     1: "The provided prefix was invalid"
+# tags:
+#   - "changes env"
+bg.env.clear_vars_with_prefix() {
+  local -a required_args=( 'prefix' )
+  local prefix
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+
+  # Check that prefix is not empty
+  [[ -z "$prefix" ]] \
+    && printf '%s\n' "ERROR: arg1 (prefix) is empty but is required" >&2 \
+    && return 1
+
+  # Check that prefix is a valid variable name
+  if ! bg.str.is_valid_var_name "$prefix"; then \
+    printf '%s\n' "ERROR: '$prefix' is not a valid variable prefix" >&2
+    return 1
+  fi
+
+  eval 'local -a vars_with_prefix=( ${!'"$prefix"'@} )'
+  # shellcheck disable=SC2156
+  for var in "${vars_with_prefix[@]}"; do
+    unset "$var"
+  done
+}
+
+# description: |
+#   returns 0 if the given string is the name of the a shell option that is 
+#   currently turned on through the 'set -o [option name]' command. This command
+#   only works with long option names.
+# inputs:
+#   stdin:
+#   args:
+#     1: "option to evaluate"
+# outputs:
+#   stdout:
+#   stderr:
+#   return_code:
+#     0: "when the string is a valid shell option in the current bash"
+#     1: "when the string is not a valid shell option in the current bash"
+# tags:
+#   - "option decorators"
+bg.env.is_shell_opt_set() ( 
+  local opt_name
+  local opt_name_iterator
+  local opt_value
+  local is_valid_opt=""
+
+  local -a required_args=( 'opt_name' )
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+
+  while IFS=$' \t\n' read -r opt_name_iterator opt_value; do
+    if [[ "$opt_name" == "$opt_name_iterator" ]]; then
+      is_valid_opt="true"
+      if [[ "$opt_value" == "on" ]]; then
+        return 0
+      fi
+    fi
+  done < <(set -o 2>/dev/null)
+
+  # Print error message to stdout if given option is not valid
+  [[ -z "$is_valid_opt" ]] \
+    && echo "'$opt_name' is not a valid shell option" >&2 \
+    && return 2
+  return 1
+)
+
+# description: |
+#   Returns the name of the parent routine of the
+#   currently executing function, where the currently
+#   executing function is the function that called
+#   bg.env.get_parent_routine_name
+# inputs:
+#   stdin: null 
+#   args: null
+# outputs:
+#   stdout: null
+#   stderr: null
+#   return_code:
+#     0: "always" 
+# tags:
+#   - "std"
+bg.env.get_parent_routine_name() {
+  # If calling function is running at top-level
+  # or if calling routine is the top-level 'main'
+  # routine, return the name of the script
+  if [[ "${#FUNCNAME[@]}" -le 3 ]]; then
+    printf "%s" "$( basename "${BASH_SOURCE[1]}" )"
+  else
+    printf "%s" "${FUNCNAME[2]}"
+  fi
+}
+
+################################################################################
+# TRAP FUNCTIONS
+################################################################################
 
 ################################################################################
 # description: |
@@ -550,50 +669,6 @@ bg.trap.clear_all() {
   done
 }
 
-################################################################################
-# description: |
-#   Clears all variables in the environment that start with the given prefix.
-#   Will unset global shell variables, as well as local and environment 
-#   variables. Will return an error if the provided prefix is not a valid
-#   function name (i.e. it's empty or not composed entirely of alphanumeric
-#   characters and underscores)
-# inputs:
-#   stdin:
-#   args:
-#     1: "prefix"
-# outputs:
-#   stdout:
-#   stderr: "Error message if prefix is not valid"
-#   return_code:
-#     0: "All variables with the given prefix were unset from the environment"
-#     1: "The provided prefix was invalid"
-# tags:
-#   - "changes env"
-################################################################################
-bg.env.clear_vars_with_prefix() {
-  local -a required_args=( 'prefix' )
-  local prefix
-  if ! bg.in.require_args "$@"; then
-    return 2
-  fi
-
-  # Check that prefix is not empty
-  [[ -z "$prefix" ]] \
-    && printf '%s\n' "ERROR: arg1 (prefix) is empty but is required" >&2 \
-    && return 1
-
-  # Check that prefix is a valid variable name
-  if ! bg.str.is_valid_var_name "$prefix"; then \
-    printf '%s\n' "ERROR: '$prefix' is not a valid variable prefix" >&2
-    return 1
-  fi
-
-  eval 'local -a vars_with_prefix=( ${!'"$prefix"'@} )'
-  # shellcheck disable=SC2156
-  for var in "${vars_with_prefix[@]}"; do
-    unset "$var"
-  done
-}
 
 ################################################################################
 # description: Checks if the shell program running this process is bash 
@@ -695,50 +770,6 @@ bg.func.is_declared() (
 )
 
 
-#################################################################################
-# description: |
-#   returns 0 if the given string is the name of the a shell option that is 
-#   currently turned on through the 'set -o [option name]' command. This command
-#   only works with long option names.
-# inputs:
-#   stdin:
-#   args:
-#     1: "option to evaluate"
-# outputs:
-#   stdout:
-#   stderr:
-#   return_code:
-#     0: "when the string is a valid shell option in the current bash"
-#     1: "when the string is not a valid shell option in the current bash"
-# tags:
-#   - "option decorators"
-################################################################################
-bg.env.is_shell_opt_set() ( 
-  local opt_name
-  local opt_name_iterator
-  local opt_value
-  local is_valid_opt=""
-
-  local -a required_args=( 'opt_name' )
-  if ! bg.in.require_args "$@"; then
-    return 2
-  fi
-
-  while IFS=$' \t\n' read -r opt_name_iterator opt_value; do
-    if [[ "$opt_name" == "$opt_name_iterator" ]]; then
-      is_valid_opt="true"
-      if [[ "$opt_value" == "on" ]]; then
-        return 0
-      fi
-    fi
-  done < <(set -o 2>/dev/null)
-
-  # Print error message to stdout if given option is not valid
-  [[ -z "$is_valid_opt" ]] \
-    && echo "'$opt_name' is not a valid shell option" >&2 \
-    && return 2
-  return 1
-)
 
 # description: |
 #   Returns the command that has been specified to run when the given signal is caught
@@ -923,31 +954,6 @@ bg.arr.from_stdin() {
 }
 
 
-# description: |
-#   Returns the name of the parent routine of the
-#   currently executing function, where the currently
-#   executing function is the function that called
-#   bg.env.get_parent_routine_name
-# inputs:
-#   stdin: null 
-#   args: null
-# outputs:
-#   stdout: null
-#   stderr: null
-#   return_code:
-#     0: "always" 
-# tags:
-#   - "std"
-bg.env.get_parent_routine_name() {
-  # If calling function is running at top-level
-  # or if calling routine is the top-level 'main'
-  # routine, return the name of the script
-  if [[ "${#FUNCNAME[@]}" -le 3 ]]; then
-    printf "%s" "$( basename "${BASH_SOURCE[1]}" )"
-  else
-    printf "%s" "${FUNCNAME[2]}"
-  fi
-}
 
 # description: |
 #   Takes a string and the name of an array and prints 
