8deaf785b446b3fe32efc226b58a3e9ff9ceaac8
Raul <raularmandozc12@gmail.com>
2024-11-08 17:39:23 -0500

    add testing for log functions

--- bgrim.bash
+++ bgrim.bash
@@ -1690,3 +1690,93 @@ bg.out.format_bold() {
 
   printf "${__BG_FORMAT_BOLD}%s${__BG_FORMAT_BLANK}\n" "$string"
 }
+
+################################################################################
+# LOG FUNCTIONS
+################################################################################
+__bg.log.log() {
+  local provided_log_level
+  local message
+  required_args=( 'provided_log_level' 'message' )
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+
+  local BG_LOG_LEVEL="${BG_LOG_LEVEL:-FATAL}"
+  local -a LOG_LEVELS=( "TRACE" "DEBUG" "INFO" "WARN" "ERROR" "FATAL" )
+
+  if ! bg.arr.is_member 'LOG_LEVELS' "$BG_LOG_LEVEL"; then
+    echo "ERROR: '${BG_LOG_LEVEL}' is not a valid log level. Valid values are: 'TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR', and 'FATAL'" >&2
+    return 1
+  fi
+
+  local -i provided_log_level_index  
+  provided_log_level_index="$(bg.arr.index_of 'LOG_LEVELS' "$provided_log_level")"
+
+  local -i env_log_level_index
+  env_log_level_index="$(bg.arr.index_of 'LOG_LEVELS' "$BG_LOG_LEVEL")"
+
+  if (( env_log_level_index <= provided_log_level_index )); then
+    printf "[%-5s][%s]: %s\n" "$provided_log_level" "$( bg.env.get_parent_script_name )" "$message" >&2
+  fi
+}
+
+bg.log.trace() {
+  local message
+  required_args=( 'message' )
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+  
+  __bg.log.log "TRACE" "$message"
+}
+
+bg.log.debug() {
+  local message
+  required_args=( 'message' )
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+  
+  __bg.log.log "DEBUG" "$message"
+}
+
+bg.log.info() {
+  local message
+  required_args=( 'message' )
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+  
+  __bg.log.log "INFO" "$message"
+}
+
+bg.log.warn() {
+  local message
+  required_args=( 'message' )
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+  
+  __bg.log.log "WARN" "$message"
+}
+
+bg.log.error() {
+  local message
+  required_args=( 'message' )
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+  
+  __bg.log.log "ERROR" "$message"
+}
+
+bg.log.fatal() {
+  local message
+  required_args=( 'message' )
+  if ! bg.in.require_args "$@"; then
+    return 2
+  fi
+  
+  __bg.log.log "FATAL" "$message"
+}
--- tests/test_log.bash
+++ tests/test_log.bash
@@ -9,13 +9,168 @@ setup_suite() {
   tst.source_lib_from_root "bgrim.bash"
 }
 
-
-test_cli.sanitize_string_escapes_any_pipe_characters_in_help_message() {
+test_cli.log_prints_message_if_env_log_level_is_lower_than_the_provided_level() {
   set -euo pipefail
-  #tst.create_buffer_files
-  #__bg.cli.sanitize_string "Directory |that will | store data" >"$stdout_file" 2>"$stderr_file"
-  #ret_code="$?"
-  #assert_equals "0" "$ret_code" "should return exit code 0"
-  #assert_equals 'Directory \|that will \| store data' "$(< "$stdout_file")"
-  #assert_equals "" "$(< "$stderr_file" )" "stderr should be empty"
+  tst.create_buffer_files
+  BG_NO_FORMAT="true"
+  BG_LOG_LEVEL="TRACE"
+  __bg.log.log "INFO" "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "[INFO ][bash_unit]: my message" "$(< "$stderr_file" )"
+}
+
+test_cli.log_prints_message_if_env_log_level_is_equal_to_the_provided_level() {
+  set -euo pipefail
+  tst.create_buffer_files
+  BG_NO_FORMAT="true"
+  BG_LOG_LEVEL="INFO"
+  __bg.log.log "INFO" "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "[INFO ][bash_unit]: my message" "$(< "$stderr_file" )"
+}
+
+test_log.log_does_not_print_message_if_env_log_level_is_higher_than_the_provided_level() {
+  set -euo pipefail
+  tst.create_buffer_files
+  BG_NO_FORMAT="true"
+  BG_LOG_LEVEL="INFO"
+  __bg.log.log "TRACE" "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "" "$(< "$stderr_file" )" "stderr should be empty"
+}
+
+test_log.log_returns_1_if_env_log_level_is_not_valid() {
+  set -uo pipefail
+  tst.create_buffer_files
+  BG_NO_FORMAT="true"
+  BG_LOG_LEVEL="TRCE"
+  __bg.log.log "INFO" "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "1" "$ret_code" "should return exit code 1"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "ERROR: 'TRCE' is not a valid log level. Valid values are: 'TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR', and 'FATAL'" "$(< "$stderr_file" )"
+}
+
+test_log.log_default_env_log_level_is_fatal1() {
+  set -uo pipefail
+  tst.create_buffer_files
+  BG_NO_FORMAT="true"
+  __bg.log.log "INFO" "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "" "$(< "$stderr_file" )"
+}
+
+test_log.log_default_env_log_level_is_fatal2() {
+  set -uo pipefail
+  tst.create_buffer_files
+  BG_NO_FORMAT="true"
+  __bg.log.log "FATAL" "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "[FATAL][bash_unit]: my message" "$(< "$stderr_file" )"
+}
+
+test_log.trace_calls_internal_log_function_with_trace_level() {
+  set -euo pipefail
+  tst.create_buffer_files
+  __bg.log.log() {
+    mock_log_level="$1"
+    mock_message="$2"
+  }
+  bg.log.trace "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "" "$(< "$stderr_file" )" "stderr should be empty"
+  assert_equals "$mock_log_level" "TRACE"
+  assert_equals "$mock_message" "my message"
+}
+
+test_log.debug_calls_internal_log_function_with_debug_level() {
+  set -euo pipefail
+  tst.create_buffer_files
+  __bg.log.log() {
+    mock_log_level="$1"
+    mock_message="$2"
+  }
+  bg.log.debug "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "" "$(< "$stderr_file" )" "stderr should be empty"
+  assert_equals "$mock_log_level" "DEBUG"
+  assert_equals "$mock_message" "my message"
+}
+
+test_log.info_calls_internal_log_function_with_info_level() {
+  set -euo pipefail
+  tst.create_buffer_files
+  __bg.log.log() {
+    mock_log_level="$1"
+    mock_message="$2"
+  }
+  bg.log.info "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "" "$(< "$stderr_file" )" "stderr should be empty"
+  assert_equals "$mock_log_level" "INFO"
+  assert_equals "$mock_message" "my message"
+}
+
+test_log.warn_calls_internal_log_function_with_warn_level() {
+  set -euo pipefail
+  tst.create_buffer_files
+  __bg.log.log() {
+    mock_log_level="$1"
+    mock_message="$2"
+  }
+  bg.log.warn "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "" "$(< "$stderr_file" )" "stderr should be empty"
+  assert_equals "$mock_log_level" "WARN"
+  assert_equals "$mock_message" "my message"
+}
+
+test_log.error_calls_internal_log_function_with_error_level() {
+  set -euo pipefail
+  tst.create_buffer_files
+  __bg.log.log() {
+    mock_log_level="$1"
+    mock_message="$2"
+  }
+  bg.log.error "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "" "$(< "$stderr_file" )" "stderr should be empty"
+  assert_equals "$mock_log_level" "ERROR"
+  assert_equals "$mock_message" "my message"
+}
+
+test_log.fatal_calls_internal_log_function_with_fatal_level() {
+  set -euo pipefail
+  tst.create_buffer_files
+  __bg.log.log() {
+    mock_log_level="$1"
+    mock_message="$2"
+  }
+  bg.log.fatal "my message" >"$stdout_file" 2>"$stderr_file"
+  ret_code="$?"
+  assert_equals "0" "$ret_code" "should return exit code 0"
+  assert_equals "" "$(< "$stdout_file")"
+  assert_equals "" "$(< "$stderr_file" )" "stderr should be empty"
+  assert_equals "$mock_log_level" "FATAL"
+  assert_equals "$mock_message" "my message"
 }
