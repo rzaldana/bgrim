f932280988771f0ae6c9691f9291e02a407b448c
Raul Zaldana <rzaldana@ci.com>
2024-11-14 14:39:55 -0700

    r

--- bgrim.bash
+++ bgrim.bash
@@ -1433,55 +1433,6 @@ bg.str.escape_single_quotes() (
 )
 
 if [[ -n "${__BG_TEST_MODE:-}" ]]; then
-  source trap.bash
-  source str.bash
-fi
-
-export __BG_MKTEMP="mktemp"
-
-# description: |
-#   Creates a temporary file using 'mktemp' and sets an EXIT trap for
-#   the file to be deleted upon exit of the current shell process. It
-#   takes the name of a variable as it's only argument and places the
-#   path to the temporary file in the variable whose name is provided.
-# inputs:
-#   stdin:
-#   args:
-#     1: filename variable
-# outputs:
-#   stderr:
-#   return_code:
-#     0: "if the file was successfully created and the exit trap set"
-#     1: "if there was an error while creating the file or setting the trap"
-# dependencies:
-# tags:
-#   - "error_handling" 
-bg.tmpfile.new() {
-  local filename_var
-  local -a required_args=( 'filename_var' )
-  if ! bg.in.require_args "$@"; then
-    return 2
-  fi
-
-  # Validate that filename_var is a valid variable name
-  if ! bg.str.is_valid_var_name "$filename_var"; then
-    bg.err.print "'$filename_var' is not a valid variable name"
-    return 1
-  fi
-
-  local tmpfile_name
-  tmpfile_name="$("$__BG_MKTEMP")" \
-    || { bg.err.print "Unable to create temporary file"; return 1; }
-  bg.trap.add "rm -f '$tmpfile_name'" 'EXIT' \
-    || { bg.err.print "Unable to set exit trap to delete file '$tmpfile_name'"; return 1; }
-
-  # Assign name of tmpfile to variable whose name is contained in
-  # filename_var
-  eval "$filename_var=$tmpfile_name"
-}
-
-
-if [[ -n "${__BG_TEST_MODE:-}" ]]; then
   source in.bash
 fi
 
@@ -1705,7 +1656,7 @@ __bg.tty.tty() {
     return 1
   fi
 
-  if bg.var.is_set 'BG_NO_FORMAT'; then
+  if bg.var.is_set 'BG_NO_TTY'; then
     printf '%s\n' "${string}"
     return 0
   fi
